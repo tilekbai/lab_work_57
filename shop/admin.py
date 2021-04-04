0952097064088eadc74763d7619925e131aebf0d
tilekbai <kukushokk@gmail.com>
2021-04-04 22:31:02 +0600

    Создана модель Product. Создана миграция.

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from shop.models import Product
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "description", "category", "remainder", "cost"]
+    list_filter = ["id", "name", "category"]
+    search_fields = ["id", "name", "category"]
+    fields = ["id", "name", "category", "description", "remainder", "cost"]
+    readonly_fields = ["id"]
+
+admin.site.register(Product, ProductAdmin)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.forms import ModelChoiceField
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    description = models.CharField(max_length=2000, null=True, blank=True)
+    category = models.CharField(max_length=30, blank=False, null=False, default="other")
+    remainder = models.IntegerField(validators=[MinValueValidator(1)])
+    cost = models.DecimalField(max_digits=7, decimal_places=2)
+
+    class Meta:
+        db_table = "products"
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+    def __str__(self):
+        return f"{self.name}. {self.description}. {self.category}. {self.remainder}. {self.cost}"
+
+
